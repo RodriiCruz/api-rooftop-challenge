9f558a61379fc643e6117c11193244dd6c5e1051
Rodrigo Cruz <rodriikc@gmail.com>
2022-03-08 22:39:37 -0300

    feat: desactivo cors

---
+++ src/main/java/api/rooftop/challenge/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package api.rooftop.challenge.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *
+ * @author Rodrigo Cruz <rodriikc@gmail.com>
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*");
+    }
+}
