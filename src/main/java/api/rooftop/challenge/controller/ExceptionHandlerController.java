f6ee3c7d44929adb3488979ba1f4e347594914b3
Rodrigo Cruz <rodriikc@gmail.com>
2022-03-11 21:47:06 -0300

    feat: implemento manejo de excepciones codigo http 405

--- src/main/java/api/rooftop/challenge/controller/ExceptionHandlerController.java
+++ src/main/java/api/rooftop/challenge/controller/ExceptionHandlerController.java
@@ -5,9 +5,11 @@ import api.rooftop.challenge.exceptions.NotFoundException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 /**
@@ -17,13 +19,24 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class ExceptionHandlerController extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(value = {NotFoundException.class})
+    @ExceptionHandler(value = {NotFoundException.class, MethodArgumentTypeMismatchException.class})
     protected ResponseEntity<Object> notFoundException(RuntimeException ex, WebRequest request) {
         ErrorDTO errorDTO = ErrorDTO.builder()
                 .error(true)
                 .message("Text not found")
                 .code(404)
                 .build();
+        return handleExceptionInternal(ex, errorDTO, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+
+    @Override//405
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(final HttpRequestMethodNotSupportedException ex, final HttpHeaders headers, final HttpStatus status, final WebRequest request) {
+        ErrorDTO errorDTO = ErrorDTO.builder()
+                .error(true)
+                .message("Page not found")
+                .code(404)
+                .build();
+
         return handleExceptionInternal(ex, errorDTO, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
